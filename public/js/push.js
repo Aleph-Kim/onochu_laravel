44cd9ae8b70735af225ca5774fe81eef158b68f5
Aleph kim <dktjdej@naver.com>
2026-07-29 15:01:06 +0900

    fix: 신곡 알림 최초 구독 시 오류 발생 수정

--- public/js/push.js
+++ public/js/push.js
@@ -58,7 +58,8 @@ async function subscribePush() {
         return false;
     }
 
-    const registration = await navigator.serviceWorker.register('/sw.js');
+    await navigator.serviceWorker.register('/sw.js');
+    const registration = await navigator.serviceWorker.ready;
     const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: urlBase64ToUint8Array(getMeta('vapid-public-key')),
